7039a19ddece9b9f2bd0649471e9283be919fc92
Roxfreeze <53863293+Roxfreeze@users.noreply.github.com>
2019-08-08 00:56:16 +0800

    使用方式
    1.下載上述所有檔案或者下載 Credo.xml 並在Microsoft sotre下載一eSheep 64bit https://www.microsoft.com/zh-tw/p/esheep-64bit/9mx2v0tqt6rm?rtc=1&activetab=pivot:overviewtab 因為我經過了數天的各種折磨之後，已經不確定我電腦上的羊羊是不是在混亂中被改成了奇美拉。
    2.執行eSheep，當預設的羊出現之後，將Xml檔案拖曳到牠身上。
    3.以上，也許之後會更新其他角色的檔案，但是要看我的肝臟撐不撐得住，他大老爺高興一切好說。

---
+++ uninstall.cmd
@@ -0,0 +1,18 @@
+
+@echo on 
+setlocal 
+cd %~dp0\.. 
+:: Remove running application
+taskkill /IM DesktopPet.exe /f
+:: Delete the shortcuts 
+del "C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\DesktopPet.url"
+del "C:\Users\user\Desktop\DesktopPet.url"
+del "C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\DesktopPet.url"
+ 
+:: Use start to finish before we're deleted 
+start /min "" cmd /c
+%= Use reg to delete the app key =%
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\DesktopPet" /f
+%= Remove the app's root folder =%
+rd /s /q "DesktopPet"
+            
